fd3dae0f3703f0ee333f4411bb6fda3390d9d7e5
xuld <xuld@vip.qq.com>
2011-12-22 11:32:20 +0800

    按钮

--- assets/project/uplus.js
+++ assets/project/uplus.js
@@ -13,7 +13,7 @@ var menus = {
 	Core: '+Index Icon',
 	Layout: '+Index +Box',
 	Form: '+TextBox ListBox ComboBox DatePicker ProcessBar',
-	Button: 'Index ImageButton VedioButton',
+	Button: '+Index ImageButton VedioButton',
 	Dialog: 'Index',
 	Display: '+TipBox +Face Image Pager Navigator Ad',
 	Widget: 'Index +Topbar'
