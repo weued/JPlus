d567128a0ebc5bc4a8be87abbf9f226db7193358
xuld <xuld@vip.qq.com>
2011-12-28 16:19:19 +0800

    新增组件

--- assets/project/uplus.js
+++ assets/project/uplus.js
@@ -12,9 +12,12 @@ System.copyright = '';
 var menus = {
 	Core: '+Index Icon +ContentControl +IContainerControl +ListControl ScrollableControl',
 	Layout: '+Index# +Box#',
-	Form: '+TextBox *CombinedTextBox *ListBox *ComboBox *TagChooser DatePicker ProcessBar',
-	Button: '+Index ImageButton VedioButton LinkButton',
-	Dialog: 'Index',
-	Display: '+TipBox +Face +Image Pager Navigator +ImageBox +Ad +Banner *ToolTip BalloonTip',
-	Widget: 'Index +Topbar'
+	Form: '+TextBox *CombinedTextBox *ListBox *ComboBox *TagChooser DatePicker *ProcessBar *Suggest CityChooser',
+	Button: '+Index ImageButton VedioButton LinkButton *SafeAnthor',
+	Container: '*Dialog',
+	Display: '+TipBox +Face +Image Pager -Navigator +ImageBox +Ad +Banner *ToolTip -BalloonTip',
+	Widget: 'Index +Topbar *BreadCrumb +ShareTo'
 };
+
+
+
