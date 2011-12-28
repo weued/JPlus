306f54a6a9480281686f5c6d9fc44008e68c256e
xuld <xuld@vip.qq.com>
2011-12-28 16:39:23 +0800

    正则工具

---
+++ assets/project/controls.js
@@ -0,0 +1,17 @@
+/**
+ * @fileOverview J+ UI 项目引导文件
+ */
+
+
+var menus = {
+	Core: '+Index Icon +ContentControl +IContainerControl +ListControl ScrollableControl',
+	Layout: '+Index# +Box#',
+	Form: '+TextBox *CombinedTextBox *ListBox *ComboBox *TagChooser DatePicker *ProcessBar *Suggest CityChooser',
+	Button: '+Index ImageButton VedioButton LinkButton *SafeAnthor',
+	Container: '*Dialog',
+	Display: '+TipBox +Face +Image Pager -Navigator +ImageBox +Ad +Banner *ToolTip -BalloonTip',
+	Widget: 'Index +Topbar *BreadCrumb +ShareTo'
+};
+
+
+
