0e4b1085a0e14509e178894507e3a167c2e1292c
xuld <xuld@vip.qq.com>
2012-02-04 17:01:34 +0800

    版本恢复

--- assets/project/project.js
+++ assets/project/project.js
@@ -6,7 +6,6 @@ var navs = {
 	//'首页': 'index.html',
 	'核心': 'test/index.html',
 	'控件': 'controls/index.html',
-	'iplus': 'iplus/index.html',
 	'wplus':'wplus/index.html',
 	'资源': 'resources/index.html'
 };
