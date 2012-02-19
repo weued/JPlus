6470d1df647768e9aa6f2aa24f2cee8483fd0de6
xuld <xuld@vip.qq.com>
2012-02-19 20:32:27 +0800

    表单

--- bootstrap/project.js
+++ bootstrap/project.js
@@ -20,7 +20,7 @@ var menus = {
 	Core: '+Base',
 	Page: '+Common +Scaffolding +Grid# +Grid-Fluid# +Grid-Fixed# TopBar TopBar-White NavBar Footer Breadcrumbs Pagination Pager',
 	Display: 'Marquee Code Image ScrollSpy Icon List Label Tip TipBox ProcessBar Bubble Tooltip BalloonTip Carousel',
-	Form: 'Form TextBox Select ComboBox CheckBox RadioButton Suggest',
+	Form: '+Form +TextBox +Select +CheckBox +RadioButton Suggest',
 	Button: 'Button MenuButton SplitButton ButtonGroup Menu CloseButton ToolBar',
 	Container: 'Stackable Tabbable TabControls Dialog MessageBox Accordion Panel',
 	DataView: 'Table TreeView'
