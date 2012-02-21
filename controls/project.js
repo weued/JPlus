b4f3ad8b8aa788e5912e9efdc4dcc790ad79bca8
xuld <xuld@vip.qq.com>
2012-02-21 22:58:01 +0800

    项目计划

--- controls/project.js
+++ controls/project.js
@@ -6,10 +6,10 @@
 
 
 var menus = {
-	Core: '+Base',
+	Core: '+Base -Mask -Splitter -Resizable -Wrapper -ICollapsable -IContainerControl -IDropDownMenuContainer -IAlignedControl -ContentControl -ScrollableControl -ContainerControl -ListControl',
 	Page: '+Common +Scaffolding +Grid# +Grid-Fluid# +Grid-Fixed# TopBar TopBar-White NavBar Footer Breadcrumbs Pagination Pager',
 	Display: 'Marquee +Thumbnail ScrollSpy +Icon List Label Tip TipBox ProcessBar +Bubble Tooltip BalloonTip Carousel',
-	Form: '+Form +TextBox +Select +CheckBox +RadioButton +ListBox *Suggest',
+	Form: '+Form -IFormControl +TextBox +Select +CheckBox +RadioButton +ListBox *Suggest',
 	Button: '+Button +MenuButton SplitButton ButtonGroup +Menu +CloseButton ToolBar',
 	Container: 'Stackable Tabbable TabControls Dialog MessageBox Accordion +Panel',
 	DataView: '+Table TreeView'
