a24da3f5d5ee8de270b7646cb0766c124f0ec390
xiaoc <hustcoolboy@gmail.com>
2012-02-24 19:14:47 +0800

    test

---
+++ hust/_tools/build-all.bat
@@ -0,0 +1,2 @@
+@"../../assets/tools/DplBuilder/DplBuilder" ../../ build/css/hust.css build/js/hust.js ../images Hust.Button.testButton
+@pause
---
+++ hust/project.js
@@ -0,0 +1,15 @@
+/**
+ * @fileOverview 测试导航
+ */
+
+System.title = 'hust';
+System.subtitle = '';
+System.copyright = 'hust';
+
+
+//这个文件用来管理组件列表。
+// 命名自己来。
+var menus = {
+	Core: 'Base',
+	Button: 'ArrowButton'
+};
