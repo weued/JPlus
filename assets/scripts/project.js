d5f9b1d38b0a8a0df83899d192e1fe287f944645
xuld <xuld@vip.qq.com>
2012-02-26 15:25:25 +0800

    新建项目 - 蓝色骑士

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -6,6 +6,7 @@ var navs = {
 	//'首页': 'index.html',
 	'核心': 'system/index.html',
 	'Aqua': 'aqua/index.html',
+	'蓝色骑士': 'blue knight/index.html',
 	'控件': 'controls/index.html',
 	'hust': 'hust/index.html',
 	'wplus':'wplus/index.html',
---
+++ blue knight/project.js
@@ -0,0 +1,8 @@
+/**
+ * @fileOverview 测试导航
+ */
+
+var menus = {
+	Core: 'Base',
+	Page: 'Common Scaffolding'
+};
