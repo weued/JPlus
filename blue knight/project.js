e3e31729a35e2892f5f827299de76a290d365379
basecss <basecss@gmail.com>
2012-02-27 15:54:23 +0800

    aa

--- blue knight/project.js
+++ blue knight/project.js
@@ -4,10 +4,11 @@
 
 System.title = '蓝色骑士';
 System.subtitle = 'blue knight';
-System.copyright = 'basecss';
 
 var menus = {
 	Core: 'Base',
 	Page: 'Common Scaffolding',
-    Container: '+Panel -Dialog'
+    Form: 'Form',
+    Container: '+Panel *Dialog',
+    Framework: '-Layout'
 };
