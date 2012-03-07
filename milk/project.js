f393d2ce7c25acee9ae141ea543e8185d58316c2
xuld <xuld@vip.qq.com>
2012-03-07 21:52:39 +0800

    初步完成 milk 系列组件

--- milk/project.js
+++ milk/project.js
@@ -7,11 +7,11 @@
 
 var menus = {
 	Core: '+Base +Common -Mask -Splitter -Resizable -Wrapper -ICollapsable -IContainerControl -IDropDownMenuContainer -IAlignedControl -ContentControl -ScrollableControl -ContainerControl -ListControl',
-	Page: '*Scaffolding *Grid# *Grid-Fluid# *Grid-Fixed# TopBar TopBar-White NavBar Footer Breadcrumbs Pagination Pager',
+	Page: '+Scaffolding +Grid# +Grid-Fluid# +Grid-Fixed# -TopBar -NavBar -Footer +Breadcrumbs +Pagination +Pager',
 	Display: '-Marquee +Thumbnail -ScrollSpy +Icon -List -Label +Arrow +Tip +TipBox +ProgressBar +Bubble +ToolTip +BalloonTip',
 	Form: '+Form -IFormControl +TextBox +Select +CheckBox +RadioButton +SearchTextBox +ListBox -Suggest',
 	Button: '+Button +MenuButton +SplitButton +ButtonGroup -Menu +CloseButton -ToolBar',
-	Container: 'Stackable Tabbable TabControls Dialog MessageBox Accordion *Panel',
+	Container: '+Tabbable -TabControls -Dialog -MessageBox +Accordion +Panel',
 	DataView: '+Table -TreeView'
 };
 
