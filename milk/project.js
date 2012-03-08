473e35feb6ac85a8879eb4c0d23e91e60e96b979
xuld <xuld@vip.qq.com>
2012-03-08 20:58:58 +0800

    ico

--- milk/project.js
+++ milk/project.js
@@ -6,12 +6,12 @@
 
 
 var menus = {
-	Core: '+Base +Common -Mask -Splitter -Resizable -Wrapper -ICollapsable -IContainerControl -IDropDownMenuContainer -IAlignedControl -ContentControl -ScrollableControl -ContainerControl -ListControl',
-	Page: '+Scaffolding +Grid# +Grid-Fluid# +Grid-Fixed# -TopBar -NavBar -Footer +Breadcrumbs +Pagination +Pager',
-	Display: '-Marquee +Thumbnail -ScrollSpy +Icon -List -Label +Arrow +Tip +TipBox +ProgressBar +Bubble +ToolTip +BalloonTip',
-	Form: '+Form -IFormControl +TextBox +Select +CheckBox +RadioButton +SearchTextBox +ListBox -Suggest',
-	Button: '+Button +MenuButton +SplitButton +ButtonGroup -Menu +CloseButton -ToolBar',
-	Container: '+Tabbable -TabControls -Dialog -MessageBox +Accordion +Panel',
-	DataView: '+Table -TreeView'
+	Core: '*Base *Common -Mask -Splitter -Resizable -Wrapper -ICollapsable -IContainerControl -IDropDownMenuContainer -IAlignedControl -ContentControl -ScrollableControl -ContainerControl -ListControl',
+	Page: '*Scaffolding *Grid# *Grid-Fluid# *Grid-Fixed# -TopBar -NavBar -Footer *Breadcrumb *Pagination *Pager',
+	Display: '-Marquee *Thumbnail -ScrollSpy *Icon *IcoMoon -List -Label *Arrow *Tip *TipBox *ProgressBar *Bubble *ToolTip *BalloonTip *Line',
+	Form: '*Form -IFormControl *TextBox *Select *CheckBox *RadioButton *SearchTextBox *ListBox -Suggest',
+	Button: '*Button *MenuButton *SplitButton *ButtonGroup -Menu *CloseButton -ToolBar *LinkButton',
+	Container: '*Tabbable -TabControls -Dialog -MessageBox *Accordion *Panel',
+	DataView: '*Table -TreeView'
 };
 
