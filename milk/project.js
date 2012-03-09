d33f9c8554191fafb8a70aaaf56b0a4d3c2612b7
xuld <xuld@vip.qq.com>
2012-03-09 20:58:22 +0800

    提示框

--- milk/project.js
+++ milk/project.js
@@ -7,12 +7,12 @@
 
 var menus = {
 	Core: '+Base +Common -Mask -Splitter -Resizable -Wrapper -ICollapsable -IContainerControl -IDropDownMenuContainer -IAlignedControl -ContentControl -ScrollableControl -ContainerControl -ListControl',
-	Page: '+Scaffolding +Grid# +Grid-Fluid *Grid-Fixed# -TopBar -NavBar -Footer *Breadcrumb *Pagination *Pager',
-	Display: '-Marquee *Thumbnail -ScrollSpy *Icon *IcoMoon -List -Label *Arrow *Tip *TipBox *ProgressBar *Bubble *ToolTip *BalloonTip *Line',
+	Page: '+Scaffolding +Grid# +Grid-Fluid +Grid-Fixed# -TopBar -NavBar -Footer +Breadcrumb +Pagination +Pager',
+	Display: '-Marquee +Thumbnail +Icon -IcoMoon -List -Label +Arrow +Tip +TipBox +ProgressBar +Bubble +ToolTip+BalloonTip +Line',
 	Form: '+Form -IFormControl +TextBox +Select +CheckBox +RadioButton +SearchTextBox +ListBox -Suggest',
 	Button: '+Button +MenuButton +SplitButton +ButtonGroup -Menu +CloseButton -ToolBar +LinkButton',
-	Container: '+Tabbable +Stackable -TabControls -Dialog -MessageBox *Accordion *Panel',
-	DataView: '*Table -TreeView',
-	Test: 'Verrical-Align'
+	Container: '+Tabbable +Stackable +TabControls -Dialog -MessageBox +Accordion +Panel',
+	DataView: '+Table -TreeView',
+	Test: '-Verrical-Align'
 };
 
