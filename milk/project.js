af756619e749ecf694e1fcd1252ba7b0895a8709
xuld <xuld@vip.qq.com>
2012-03-14 18:22:07 +0800

    布局

--- milk/project.js
+++ milk/project.js
@@ -7,8 +7,8 @@
 
 var menus = {
 	Core: '+Base +Common +ScrollableControl -ContainerControl +ListControl -Mask -Splitter -Resizable -Wrapper -ICollapsable +IDropDownMenuContainer +ContentControl',
-	Page: '+Scaffolding +Grid# +Grid-Fluid +Grid-Fixed# -TopBar -NavBar -Footer +Breadcrumb +Pagination +Pager',
-	Display: '+IToolTip +Thumbnail +Icon -IcoMoon -List -Label +Arrow +Tip +TipBox +ProgressBar +Bubble *ToolTip *BalloonTip +Line',
+	Page: '+Scaffolding +Scaffolding-Absolute +Grid# +Grid-Fluid +Grid-Fixed# -TopBar -NavBar -Footer +Breadcrumb +Pagination +Pager',
+	Display: '+IToolTip +Thumbnail +Icon -IcoMoon -List -Label +Arrow +Tip +TipBox +ProgressBar +Bubble +ToolTip *BalloonTip +Line',
 	Form: '+IInput +Form +TextBox +TextArea +CombinedTextBox +ComboBox +Select +CheckBox +RadioButton +SearchTextBox +ListBox +Suggest',
 	Button: '*Button *MenuButton *SplitButton +ButtonGroup -Menu +CloseButton -ToolBar +LinkButton',
 	Container: '+Tabbable +Stackable *TabControls -Dialog -MessageBox *Accordion *Panel',
