19c9ead5a7ad012008f5a9f0b5f2741457ea641f
xuld <xuld@vip.qq.com>
2012-03-15 15:44:11 +0800

    提示框

---
+++ system/core/assets/scripts/jquery-style.js
@@ -0,0 +1,1 @@
+/** * @author  */using("System.Dom.Element");Control.implement({		css: function(name, value){		if(arguments.length == 1) {			if(typeof name === 'string')				return this.getStyle(name);			for(var item in name)				this.setStyle(item, name[item]);			return this;		}					return this.setStyle(name, value);	},});
