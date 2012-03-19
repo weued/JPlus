574c7d4d2f85ea7d18e500a5a8b71e6156644332
xuld <xuld@vip.qq.com>
2012-03-19 16:30:08 +0800

    玩晒  listcontrol

--- milk/container/assets/scripts/tabbable.js
+++ milk/container/assets/scripts/tabbable.js
@@ -1,1 +1,1 @@
-/** * @author  */using("Controls.Core.ListControl");var Tabbable = ListControl.extend({		xType: 'tabbable',		init: function(){		this.base('init');		this.controls.each(function(value){			this.getChildContainer(value).on('click', function(){				this.setSelectedItem(value)			}, this);		}, this);	}});
+/** * @author  */using("Controls.Core.ListControl");var Tabbable = ListControl.extend({		xType: 'tabbable',		init: function(){		this.base('init');				this.bindSelector('click');	}});
