6e7dd75d450768f4eb46084dc11ff8a15d4ec679
xuld <xuld@vip.qq.com>
2012-04-04 21:29:31 +0800

    菜单

--- milk/project.js
+++ milk/project.js
@@ -10,9 +10,9 @@ var menus = {
 	Page: '+Scaffolding +Scaffolding-Absolute# +Grid# +Grid-Fluid +Grid-Fixed# +Breadcrumb +Pagination +Pager -TopBar -NavBar -Footer',
 	Display: '+IToolTip +Thumbnail +Icon +List +Arrow +Tip +TipBox +ProgressBar +Bubble +ToolTip +BalloonTip +Line +IcoMoon +Label +Mask',
 	Form: '+IInput +Form +TextBox +TextArea +CombinedTextBox +ComboBox +Select +CheckBox +RadioButton +ListBox +Suggest +Validator +TagPicker +MonthCalender +DatePicker +FileUpload -SearchTextBox',
-	Button: '+Button +MenuButton +SplitButton +ButtonGroup +CloseButton +LinkButton +Menu +ContextMenu -ToolBar',
+	Button: '+Button +MenuButton +SplitButton +ButtonGroup +CloseButton +LinkButton +Menu +Menu-Alt +ContextMenu -ToolBar',
 	Container: '+Tabbable +Stackable *TabControl +Accordion +Panel +Dialog -MessageBox',
-	DataView: '+Table -TreeView',
+	DataView: '+Table *TreeView',
 	Test: '+Vertical-Align'
 };
 
